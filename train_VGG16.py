c5c9ae2894d5bc63ab468158b728c8c120f12939
mohsen-azimi <azimi.eng@gmail.com>
2022-12-04 16:29:19 +0300

    updated

--- train_VGG16.py
+++ train_VGG16.py
@@ -212,11 +212,23 @@ for epoch in range(NUM_EPOCHS):
     val_loss_list.append(epoch_val_loss)
     val_acc_list.append(epoch_val_acc)
 
-    # save the lists to txt files
-    np.savetxt('train_loss_list.txt', train_loss_list)
-    np.savetxt('train_acc_list.txt', train_acc_list)
-    np.savetxt('val_loss_list.txt', val_loss_list)
-    np.savetxt('val_acc_list.txt', val_acc_list)
+    # save the lists to files
+    with open('train_loss_list.txt', 'w') as f:
+        for item in train_loss_list:
+            f.write("%s\n" % item)
+    with open('train_acc_list.txt', 'w') as f:
+        for item in train_acc_list:
+            f.write("%s\n" % item)
+
+    with open('val_loss_list.txt', 'w') as f:
+        for item in val_loss_list:
+            f.write("%s\n" % item)
+
+    with open('val_acc_list.txt', 'w') as f:
+        for item in val_acc_list:
+            f.write("%s\n" % item)
+
+
 
 
 
