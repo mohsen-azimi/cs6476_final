d7730626499d4e49edd5b127f5a5bdc208ef9c8f
mohsen-azimi <azimi.eng@gmail.com>
2022-12-06 19:35:41 +0300

    updated

--- models.py
+++ models.py
@@ -1,92 +1,115 @@
 
+import torch
 import torch.nn as nn
 
 
 class MyModel(nn.Module):
-
     def __init__(self):
         super(MyModel, self).__init__()
 
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=(3, 3), stride=(1, 1), padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.2)
-        )
+        #      , , ]
+        layers = []
+        # 64, 64, 'M'
+        layers += [nn.Conv2d(3, 64, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(64),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(64, 64, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(64),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        layers += [nn.Dropout(0.2)]
 
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(1, 1), padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.2)
-        )
 
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.2)
-        )
 
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.2)
-        )
+        # 128, 128, 'M'
+        layers += [nn.Conv2d(64, 128, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(128),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(128, 128, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(128),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        layers += [nn.Dropout(0.2)]
 
-        self.conv5 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.2)
-        )
+       #  256, 256, 256, 256, 'M'
+        layers += [nn.Conv2d(128, 256, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(256),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(256, 256, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(256),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(256, 256, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(256),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(256, 256, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(256),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        layers += [nn.Dropout(0.2)]
 
-        self.conv6 = nn.Sequential(
-            nn.Conv2d(512, 1024, kernel_size=(3, 3), stride=(1, 1), padding=1),
-            nn.BatchNorm2d(1024),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.2)
-        )
+        # 512, 512, 512, 512, 'M'
+        layers += [nn.Conv2d(256, 512, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(512),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(512),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(512),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(512),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        layers += [nn.Dropout(0.2)]
 
-        self.conv7 = nn.Sequential(
-            nn.Conv2d(1024, 2048, kernel_size=(3, 3), stride=(1, 1), padding=1),
-            nn.BatchNorm2d(2048),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.2)
-        )
+        # 512, 512, 512, 512, 'M'
+        layers += [nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(512),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(512),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(512),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1),
+                   nn.BatchNorm2d(512),
+                   nn.ReLU(inplace=True)]
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        layers += [nn.Dropout(0.2)]
 
-        self.fc1 = nn.Sequential(
-            nn.Linear(2048 * 2 * 2, 1024),
-            nn.ReLU(),
-            nn.Dropout(0.2)
-        )
 
-        self.fc2 = nn.Sequential(
-            nn.Linear(1024, 512),
-            nn.ReLU(),
-            nn.Dropout(0.2)
-        )
+        # average pooling
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
 
-        self.output = nn.Sequential(nn.Linear(512, 11))
+
+        self.features = nn.Sequential(*layers)
+        self.classifier = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(True),
+            nn.Dropout(0.2),
+            nn.Linear(256, 128),
+            nn.ReLU(True),
+            nn.Dropout(0.2),
+            nn.Linear(128, 11),
+        )
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.conv6(x)
-        x = self.conv7(x)
+        x = self.features(x)
         x = x.view(x.size(0), -1)
-        x = self.fc1(x)
-        x = self.fc2(x)
-        x = self.output(x)
+        x = self.classifier(x)
         return x
+
+
+
+
+if __name__ == '__main__':
+    model = MyModel()
+    inputs = torch.randn(5, 3, 32, 32)
+    output = model(inputs)
+
+    print(output.size())
+
+
+
